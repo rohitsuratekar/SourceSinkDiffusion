42c4e3ed832ddb0b2e496b41e2dcc22bb93f13d6
Rohit Suratekar <rohitsuratekar@gmail.com>
2015-11-16 14:18:11 +0530

    Improved plotting.

--- SSD_functions.py
+++ SSD_functions.py
@@ -3,16 +3,18 @@ import SSD_settings as ss
 from random import randrange
 from numpy import cos, sin, radians
 
+#Randomization with bounce from boundary conditions
 def GoRandom(x,y,step):
-    angle = radians(randrange(360))
-    new_x = x + step*cos(angle)
-    new_y = y + step*sin(angle)
+    angle = radians(randrange(360))  #Generate Random direction
+    new_x = x + step*cos(angle)  #Convert to cartesian
+    new_y = y + step*sin(angle)  #Convert to cartesian
+    #Check for boundry conditions. IF outside boundary, take different random direction.
     if new_x < ss.grid_width[0]:
         while new_x <= ss.grid_width[0]:
             angle = radians(randrange(360))
             new_x = x + step*cos(angle)
     if new_x > ss.grid_width[1]:
-        while new_x >= ss.grid_width[0]:
+        while new_x >= ss.grid_width[1]:
             angle = radians(randrange(360))
             new_x = x + step*cos(angle)
     if new_y < ss.grid_hight[0]:
@@ -23,5 +25,4 @@ def GoRandom(x,y,step):
         while new_y >= ss.grid_hight[1]:
             angle = radians(randrange(360))
             new_y = y + step*sin(angle)
-
     return new_x, new_y
--- SSD_main.py
+++ SSD_main.py
@@ -7,16 +7,20 @@ matplotlib.use('TkAgg') #For interactive plots
 import numpy as np
 import matplotlib.pyplot as plt
 
-starting_point1 = [25,25]
-starting_point2 = [25,25]
-plt.ion()
-graph = plt.plot(starting_point1[0],starting_point1[1])
-plt.ylim([0,100])
-plt.xlim([0,100])
-for i in range(100):
-    starting_point1 = sfuc.GoRandom(starting_point1[0],starting_point1[1],10)
-    starting_point2 = sfuc.GoRandom(starting_point2[0],starting_point2[1],10)
+starting_point1 = [50,50]
+starting_point2 = [50,50]
+
+fig = plt.figure()
+for i in range(50):
+    grid = np.zeros(shape=(ss.grid_width[1],ss.grid_hight[1]))
+    starting_point1 = sfuc.GoRandom(starting_point1[0],starting_point1[1],20)
+    starting_point2 = sfuc.GoRandom(starting_point2[0],starting_point2[1],20)
+    grid[starting_point1[0]][starting_point1[1]] = 1
+    grid[starting_point2[0]][starting_point2[1]] = 1
     plt.scatter(starting_point1[0],starting_point1[1],color='r')
     plt.scatter(starting_point2[0],starting_point2[1],color='b')
+    plt.ylim([0,100])
+    plt.xlim([0,100])
     plt.draw()
-    plt.pause(0.001)
+    plt.pause(0.00001)
+    plt.clf()
