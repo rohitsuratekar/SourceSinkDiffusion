9aa3851bda71132ff8308b0f82c6f9b87ff698fa
Rohit Suratekar <rohitsuratekar@gmail.com>
2015-11-16 17:15:25 +0530

    Working copy with separate radius, color and position

--- SSD_main.py
+++ SSD_main.py
@@ -2,25 +2,36 @@
 #November 2015
 import SSD_settings as ss
 import SSD_functions as sfuc
-import math, random, time, matplotlib
+from matplotlib.patches import Circle
+import math, random, time, matplotlib, colorsys
 matplotlib.use('TkAgg') #For interactive plots
 import numpy as np
 import matplotlib.pyplot as plt
 
-starting_point1 = [50,50]
-starting_point2 = [50,50]
+N = ss.number_of_substarte_molecules
+HSV_tuples = [(x*1.0/N, 0.5, 0.5) for x in range(N)]
+RGB_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)
 
-fig = plt.figure()
+molecules =[]
+#Initialize starting points of molecules
+for m in range(ss.number_of_substarte_molecules):
+    molecules.append((random.uniform(ss.grid_width[0],ss.grid_width[1]), random.uniform(ss.grid_hight[0],ss.grid_hight[1]), ss.molecule_radius, RGB_tuples[m]))
+
+plt.axes()
+
 for i in range(50):
-    grid = np.zeros(shape=(ss.grid_width[1],ss.grid_hight[1]))
-    starting_point1 = sfuc.GoRandom(starting_point1[0],starting_point1[1],20)
-    starting_point2 = sfuc.GoRandom(starting_point2[0],starting_point2[1],20)
-    grid[starting_point1[0]][starting_point1[1]] = 1
-    grid[starting_point2[0]][starting_point2[1]] = 1
-    plt.scatter(starting_point1[0],starting_point1[1],color='r')
-    plt.scatter(starting_point2[0],starting_point2[1],color='b')
-    plt.ylim([0,100])
-    plt.xlim([0,100])
+    random.shuffle(molecules)  #Randomize molecue order
+    for element in range(len(molecules)):
+        temp_store = molecules[element]
+        temp_store2 = sfuc.GoRandom(temp_store[0],temp_store[1],2)
+        molecules[element] =[temp_store2[0], temp_store2[1], temp_store[2], temp_store[3]]
+
+    for element in molecules:
+        c = plt.Circle((element[0],element[1]),element[2], color = element[3] )
+        plt.gca().add_patch(c)
+
+    plt.xlim([ss.grid_width[0],ss.grid_width[1]])
+    plt.ylim([ss.grid_hight[0],ss.grid_hight[1]])
     plt.draw()
     plt.pause(0.00001)
     plt.clf()
--- SSD_settings.py
+++ SSD_settings.py
@@ -2,3 +2,6 @@
 #Grid Properties
 grid_hight = (0,100)
 grid_width = (0,100)
+
+number_of_substarte_molecules = 10
+molecule_radius = 1
